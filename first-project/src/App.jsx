7815d5456b0d26bb5a5dafd0a9a44e5725c86921
hmsami4547 <hmsami4547@gmail.com>
2025-10-06 22:27:22 +0600

    first commit

---
+++ first-project/src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+import Countries from '../../Countries'
+
+function App() {
+
+
+  return (
+    <>
+    <div>
+      <h1>Let's start with react</h1>
+      <Countries></Countries>
+    </div>
+    </>
+  )
+}
+
+export default App
